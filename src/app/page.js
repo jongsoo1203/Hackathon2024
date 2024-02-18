8f03050616b70b64321b255619a5abad85c6bc6a
jongsoo1203 <114615490+jongsoo1203@users.noreply.github.com>
2024-02-18 15:26:08 -0500

    fetch UMBInvolved

--- src/app/page.js
+++ src/app/page.js
@@ -2,37 +2,45 @@
 import styles from "./page.module.css";
 import React, { useState, useEffect } from "react"; // Import useState and useEffect
 
-const dummyData = {
-  events: [
-    {
-      id: 1,
-      name: "Event 1",
-      date: "2024-02-18T11:28:16-05:00"
-    },
-    {
-      id: 2,
-      name: "Event 2",
-      date: "2024-02-18T11:28:16-05:00"
-    },
-    {
-      id: 3,
-      name: "Event 3",
-      date: "2024-02-18T11:28:16-05:00"
-    }
-  ]
-};
-
-
 async function fetchEvents(query) {
   const data = {
     query: query
   };
   
   const searchParams = new URLSearchParams(data);
+  
+  // searchParams.toString() === 'var1=value1&var2=value2'
+  const url = `https://umb.campuslabs.com/engage/api/discovery/event/search?&${searchParams.toString()}`;
+
+  // return new Promise((resolve, reject) => {
+  //   fetch(
+  //     url,
+  //     {
+  //       mode: 'no-cors',
+  //     }
+  //   ).then(response => {
+  //     return response.json();
+  //   }).then(data => {
+  //     resolve(data);
+  //   });
+  // });
+
+  const response = await fetch(
+    url,
+    {
+      mode: 'no-cors',
+    }
+  )
+
+  return await response.json();
 }
 
+fetchEvents("hackathon").then(data => {console.log(data)});
+
+
+
 export default function Events() {
-  const [events, setEvents] = useState(dummyData.events);
+  // const [events, setEvents] = useState(dummyData.events);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -54,12 +62,14 @@ export default function Events() {
   return (
     <div>
       {/* Other server-side or static content */}
-      {events.map(event => (
-        <div key={event.id}>
-          <h2>{event.name}</h2>
-          <p>{event.date}</p>
-        </div>
-      ))}
+      {
+      // events.map(event => (
+      //   <div key={event.id}>
+      //     <h2>{event.name}</h2>
+      //     <p>{event.date}</p>
+      //   </div>
+      // ))
+      }
     </div>
   );
 }
