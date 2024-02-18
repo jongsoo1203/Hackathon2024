69ba3d86e4dba8fa5cd900ff49679215bdd8584d
max_moskalenko <mmoskalenko21@gmail.com>
2024-02-18 14:16:51 -0500

    calendar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Calendar from "./Calendar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      hello
+      <Calendar />
     </main>
   );
 }
